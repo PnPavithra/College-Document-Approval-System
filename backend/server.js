d705a92ae0db1dac0dda4a5ac806cdccdce55c82
Nihamo <138553475+Nihamo@users.noreply.github.com>
2025-06-02 15:01:35 +0530

    update3

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use('/api', guideRoutes);
 mongoose.connect(process.env.MONGO_URI)
    .then(() => {
     console.log("✅ MongoDB Connected");
-    app.listen(process.env.PORT, '0.0.0.0', () => {
+    app.listen(process.env.PORT, () => {
       console.log(`🚀 Server running on port ${process.env.PORT}`);
     });
   })
